b72d1e447b65bdadf29bef2aa2705c48963456d1
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-02-05 12:29:40 -0500

    refactor integration testing

--- test/integration/config/config_unit/default_test.rb
+++ test/integration/config/config_unit/default_test.rb
@@ -1,4 +1,4 @@
-title "Default role integrated test file"
+title "Systemd unit configuration integration tests"
 
 describe service('test-service') do
   it { should be_installed }
@@ -32,18 +32,4 @@ describe file('/run/systemd/system/tmp-stdin.mount') do
   its('content') { should match("Description=") }
   its('content') { should match("What=") }
   its('content') { should match("WantedBy=") }
-end
-
-describe service('type-only-section') do
-  it { should be_installed }
-end
-
-describe file('/etc/systemd/system/type-only-section.service') do
-  it { should exist }
-  its('owner') { should eq 'root' }
-  its('group') { should eq 'root' }
-  its('mode') { should cmp '0644' }
-  its('content') { should_not match("Description") }
-  its('content') { should_not match("Install") }
-  its('content') { should match("ExecStart=") }
 end
