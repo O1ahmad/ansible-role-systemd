ca9d84b72aaa547ecb79420de0850ed1cccbbac1
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-11-28 19:55:21 -0500

    update uninstall integration tests

--- test/integration/uninstall/default_test.rb
+++ test/integration/uninstall/default_test.rb
@@ -1,9 +1,5 @@
 title "Systemd unit uninstallation integration tests"
 
-describe service('test-service') do
-  it { should_not be_installed }
-end
-
 describe file('/etc/systemd/system/test-service.service') do
   it { should_not exist }
 end
